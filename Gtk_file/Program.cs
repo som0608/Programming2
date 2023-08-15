f3b93b15a2d9a6f776d1dcaaeaeb51ccb2d2bce4
som0608 <142277004+som0608@users.noreply.github.com>
2023-08-15 19:32:35 +0200

    Update Program.cs

--- Gtk_file/Program.cs
+++ Gtk_file/Program.cs
@@ -1,6 +1,7 @@
-﻿using static System.Console;
+using static System.Console;
 using Cairo;
 using Gtk;
+
 
 
 class Program {
@@ -19,17 +20,24 @@ class Program {
         {6,0,4,2,5,0,0,1,0},
         {3,7,0,0,1,4,0,9,0}
     };
+
     private bool[,] isBlankCell = new bool[BoardSize, BoardSize];
     private bool[,] HighLightCell = new bool[BoardSize, BoardSize];
 
     private int selectedCellRow = -1;
     private int selectedCellCol = -1;
 
-    static void Main() {
+    private int BlankNumber = 0;
+
+    private bool writeAnswer = false;
+
+    static void Main(string[] args) {
+
         Application.Init();
         
         Program program = new Program();
         program.SetupUI();
+        program.Solve();
 
         Application.Run();
     }
@@ -56,10 +64,76 @@ class Program {
         drawingArea.GrabFocus();
     }
 
+    private int[,] board;
+    private int[,] answer = new int[9,9];
+
+    private void Solve() {
+        board = sudokuBoard;
+        if (SolveSudoku()) {
+            PrintBoard();
+        }else{
+            Console.WriteLine("No solution exists.");
+        }
+    }
+
+    private bool SolveSudoku() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row, col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (IsValidMove(row, col, num)) {
+                            board[row, col] = num;
+
+                            if (SolveSudoku()) {
+                                return true;
+                            }
+
+                            board[row, col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private bool IsValidMove(int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row, i] == num || board[i, col] == num) {
+                return false;
+            }
+        }
+
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (board[i, j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void PrintBoard() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                // Console.Write(board[row, col] + " ");
+                answer[row, col] = board[row, col];
+            }
+            // Console.WriteLine();
+        }
+    }
+
     private void GenerateSudokuBoard() {
         for (int i = 0; i < BoardSize; i++) {
             for (int j = 0; j < BoardSize; ++j) {
                 if (sudokuBoard[i,j] == 0) {
+                    BlankNumber++;
                     isBlankCell[i,j] = false;
                     HighLightCell[i, j] = true;
                 } else {
@@ -75,10 +149,70 @@ class Program {
         uint keyValue = (uint)key;
 
         int digit = (int)(keyValue) - 48;
-        Console.WriteLine($"Pressed digit: {digit}");
+        // Console.WriteLine($"Pressed digit: {digit}");
+        if (!isBlankCell[selectedCellRow, selectedCellCol]) {
+            BlankNumber--;
+        }
         sudokuBoard[selectedCellRow, selectedCellCol] = digit;
         isBlankCell[selectedCellRow, selectedCellCol] = true;
         drawingArea.QueueDraw();
+
+        if (BlankNumber == 0) {
+            if (isCompleted()) {
+                ShowMessageDialog("Succeeded", "Congratulations! Sudoku is solved.");
+            } else {
+                ShowRetryDialog();
+            }
+        }
+    }
+    private bool isCompleted() {
+        for (int i = 0; i < BoardSize; i++) {
+            for (int j = 0; j < BoardSize; ++j) {
+                if (sudokuBoard[i,j] != answer[i, j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void ShowMessageDialog(string title, string message) {
+        MessageDialog dialog = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, message);
+        dialog.Title = title;
+        dialog.Run();
+        dialog.Destroy();
+    }
+
+    private void ShowRetryDialog() {
+        MessageDialog dialog = new MessageDialog(null, DialogFlags.Modal, MessageType.Warning, ButtonsType.None, "Try again?");
+        dialog.AddButton("Retry", ResponseType.Yes);
+        dialog.AddButton("Answer", ResponseType.No);
+        dialog.Title = "Failed";
+        ResponseType response = (ResponseType)dialog.Run();
+        dialog.Destroy();
+
+        if (response == ResponseType.Yes) {
+            // Clear the board and reset BlankNumber here
+            ClearSudokuBoard();
+            drawingArea.QueueDraw();
+        } else {
+            writeAnswer = true;
+            drawingArea.QueueDraw();
+        }
+    }
+
+    private void ClearSudokuBoard() {
+        for (int i = 0; i < BoardSize; i++) {
+            for (int j = 0; j < BoardSize; j++) {
+                if (HighLightCell[i, j]) {
+                    isBlankCell[i, j] = false;
+                    sudokuBoard[i,j] = 0;
+                    BlankNumber++;
+                } else {
+                    isBlankCell[i,j] = true;
+                }
+            }
+        }
     }
 
     private void OnCellClick(object o, ButtonPressEventArgs args) {
@@ -115,15 +249,32 @@ class Program {
         }
 
         //Draw numbers
-        for (int i = 0; i < BoardSize; i++) {
-            for (int j = 0; j < BoardSize; j++) {
-                if (isBlankCell[i, j]) {
+        if (!writeAnswer) {
+            for (int i = 0; i < BoardSize; i++) {
+                for (int j = 0; j < BoardSize; j++) {
+                    if (isBlankCell[i, j]) {
+                        if (HighLightCell[i, j]) {
+                            cr.SetSourceRGB(0, 0, 1);
+                        } else {
+                            cr.SetSourceRGB(0, 0, 0);
+                        }
+                        DrawNumber(cr, i, j, sudokuBoard[i, j]);
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < BoardSize; i++) {
+                for (int j = 0; j < BoardSize; j++) {
                     if (HighLightCell[i, j]) {
-                        cr.SetSourceRGB(0, 0, 1);
+                        if (sudokuBoard[i,j] != answer[i, j]) {
+                            cr.SetSourceRGB(1, 0, 0);
+                        } else {
+                            cr.SetSourceRGB(0, 0, 1);
+                        }
                     } else {
                         cr.SetSourceRGB(0, 0, 0);
                     }
-                    DrawNumber(cr, i, j, sudokuBoard[i, j]);
+                    DrawNumber(cr, i, j, answer[i, j]);
                 }
             }
         }
